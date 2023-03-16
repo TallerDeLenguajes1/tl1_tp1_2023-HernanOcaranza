1fefd6594cc6a0f508eab2dcf81f81fb3f9fb5f2
“Hernan <hernanocaranza@hotmail.com.ar>
2023-03-16 19:12:15 -0300

    Se modifica el gitignore para que ignore los .exe y se sube el 3-e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 int main() {
-    printf("Hola Mundo");
+    int numero = 10;
+    int* pNumero = &numero;
     
+    printf("direccion de memoria almacenada por el puntero: %p\n", pNumero);
+    printf("direccion de memoria de la variable: %p\n", &numero);
+    printf("direccion de memoria del puntero: %p\n", &pNumero);
+    printf("tamanio del memoria de la variable: %d\n", sizeof(numero));
+    printf("el contenido del puntero: %p\n", pNumero);
+
     return 0;
 }
